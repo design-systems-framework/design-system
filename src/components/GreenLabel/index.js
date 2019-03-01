73bfbf88c1fe9352a905e7571514ef3e707af94b
Charles Lee <clee@atlassian.com>
2019-03-01 15:42:39 +1100

    green label guy added

--- src/components/GreenLabel/index.js
+++ src/components/GreenLabel/index.js
@@ -1,19 +1,18 @@
+import React from 'react';
 import Textfield from '../WhiteLabel/Textfield';
 
 function setStyles ({ appearance, isCompact, isMonospaced }) {
   return {
-    inputWrapper (style, props) => ({
+    inputWrapper: (style, props) => ({
       ...style,
-      backgroundColor: appearance === 'subtle' ? transparent : '#333',
+      backgroundColor: appearance === 'subtle' ? 'transparent' : '#333',
     }),
     input: (style, prop) => ({
       ...style,
-
     })
   }
 }
 
 export default ({ appearance, isCompact, isMonospaced, ...rest }) => {
-
   return <Textfield getStyles={setStyles({ appearance, isCompact, isMonospaced })} {...rest} />
 }
