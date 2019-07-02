55247ff3293c158a50b227050b516deb297c238c
Mitchell Hamilton <mitchell@hamil.town>
2019-07-02 10:04:21 +1000

    Add ESLint and a Circle config (#18)
    * Add ESLint and a Circle config
    
    * Rename eslint config and add npm scripts

--- site/build-manifest.js
+++ site/build-manifest.js
@@ -5,17 +5,24 @@ const glob = require('glob-promise');
 
 (async () => {
   try {
-    const files = await Promise.all(workspaces.map(async key => {
-      const globPath = path.resolve(__dirname, `../${key}`);
-      if (key === 'site') return;
-      const filePaths = await glob(globPath);
-      return filePaths.map(filePath => {
-        const { name } = require(path.resolve(filePath, './package.json'));
-        return name;
-      });
-    }));
-    const formattedFiles = files.reduce((acc,curr) => acc.concat(curr), []).filter(f => f);
-    fs.writeFileSync('manifest.json', JSON.stringify({ files: formattedFiles }, null, 1));
+    const files = await Promise.all(
+      workspaces.map(async key => {
+        const globPath = path.resolve(__dirname, `../${key}`);
+        if (key === 'site') return;
+        const filePaths = await glob(globPath);
+        return filePaths.map(filePath => {
+          const { name } = require(path.resolve(filePath, './package.json'));
+          return name;
+        });
+      })
+    );
+    const formattedFiles = files
+      .reduce((acc, curr) => acc.concat(curr), [])
+      .filter(f => f);
+    fs.writeFileSync(
+      'manifest.json',
+      JSON.stringify({ files: formattedFiles }, null, 1)
+    );
   } catch (e) {
     console.error(e);
   }
