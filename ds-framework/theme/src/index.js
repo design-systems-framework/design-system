f2c588f77d85866748b518c04a6d8bea0478ee7e
gwyneplaine <cc.lee@live.com.au>
2019-07-05 13:29:30 +1000

    return shit

--- ds-framework/theme/src/index.js
+++ ds-framework/theme/src/index.js
@@ -1,32 +1,25 @@
-import React, { createContext } from 'react';
+import React, { createContext, useContext } from 'react';
 const unary = t => t;
 
 function createTheme (themeTokens = {}) {
   const ThemeCtx = createContext(unary)
   function Consumer ({ children }) {
-    return (
-      <ThemeCtx.Consumer>
-        {tfn => {
-          const tokens = tfn(theme);
-          return props.children(tokens);
-        }}
-      </ThemeCtx.Consumer>
-    )
+    const tfn = useContext(ThemeCtx);
+    return children(tfn(theme));
   }
 
   function Provider ({ value: vfn, children }) {
+    const tfn = useContext(ThemeCtx);
+    const newTfn = (t) => tfn(vfn(t));
     return (
-      <ThemeCtx.Consumer>
-        {tfn => {
-          const newTfn = (t) => tfn(vfn(t));
-          return (
-            <ThemeCtx.Provider value={newTfn}>
-              {children}
-            </ThemeCtx.Provider>
-          )
-        }}
-      </ThemeCtx.Consumer>
-    )
+      <ThemeCtx.Provider value={newTfn}>
+        {children}
+      </ThemeCtx.Provider>
+    );
+  }
+  return {
+    Provider,
+    Consumer
   }
 }
 
