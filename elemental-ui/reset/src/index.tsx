9668232cbde57c39b0589cd2811248b2c10a7dae
Jed Watson <jed.watson@me.com>
2019-09-04 21:55:20 +1000

    Adding fixed-width fonts to reset

--- elemental-ui/reset/src/index.tsx
+++ elemental-ui/reset/src/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Global, css } from '@emotion/core';
 
+// TODO: Pull these font stacks from a theme package
+
 const resetCSS = css`
   html {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
       'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji',
       'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
     line-height: 1.5;
+  }
+  pre,
+  code,
+  kbd,
+  samp {
+    font-family: Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New',
+      monospace;
   }
 `;
 
